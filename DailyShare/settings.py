48d71755285ad24ad9252b2bea1a3d024c698c77
yusakinasa <7q5u6w5y@gmail.com>
2025-12-19 16:55:45 +0800

    settings changed

--- DailyShare/settings.py
+++ DailyShare/settings.py
@@ -113,7 +113,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# 静态文件 URL
+STATIC_URL = '/static/'
+
+# collectstatic 收集到的最终目录
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 import os
